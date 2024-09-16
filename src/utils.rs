13400d5ce90e8d28e5f10cd33ae64575ccd59f6a
Paul Sanders <paul@paulsanders.dev>
2024-09-16 14:01:20 -0400

    Fix typo in function name

--- src/utils.rs
+++ src/utils.rs
@@ -5,13 +5,13 @@ use tera::Value;
 ///
 /// - `Apache OpenDAL` => `apache_opendal`
 /// - `LanceDB` => `lancedb`
-pub fn to_snack_case(input: &str) -> String {
+pub fn to_snake_case(input: &str) -> String {
     input.to_lowercase().replace(" ", "_")
 }
 
-pub fn to_snack_case_filter(v: &Value, _: &HashMap<String, Value>) -> tera::Result<Value> {
+pub fn to_snake_case_filter(v: &Value, _: &HashMap<String, Value>) -> tera::Result<Value> {
     let input = v
         .as_str()
         .ok_or_else(|| tera::Error::msg("The value is not a string"))?;
-    Ok(to_snack_case(input).into())
+    Ok(to_snake_case(input).into())
 }
